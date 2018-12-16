5b6fc034bcfc49ed70443437d0d731c2abb4418a
oahtpham <phamthaop.9@gmail.com>
2018-12-16 16:39:46 -0500

    Done.

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,29 @@
+class GenresController < ApplicationController
+
+  def show
+    @genre = Genre.find(params[:id])
+    render :show
+  end
+
+  def new
+    @genre = Genre.new
+    render :new
+  end
+
+  def create
+    @genre = Genre.create(params.require(:genre).permit(:name))
+    redirect_to genre_path(@genre)
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    @genre.update(params.require(:genre).permit(:name))
+    redirect_to genre_path(@genre)
+  end
+
+end
---
+++ app/models/song.rb
@@ -0,0 +1,34 @@
+class Song < ActiveRecord::Base
+
+  def index
+    @songs = Songs.all
+    render :index
+  end
+
+  def show
+    @song = Song.find(params[:id])
+    render :show
+  end
+
+  def new
+    @song = Song.new
+    render :new
+  end
+
+  def create
+    @song = Song.create(params.require(:song).permit(:name, :artist_id, :genre_id))
+    redirect_to song_path(@song)
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    @song.update(params.require(:song).permit(:name, :artist_id, :genre_id))
+    redirect_to song_path(@song)
+  end
+
+end
