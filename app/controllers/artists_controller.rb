e95d4687f02bb59fb2ceefe0dc19d7b28ff479c3
aschemily <aschemily@gmail.com>
2018-12-17 15:20:32 -0500

    Done.

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,44 @@
+class ArtistsController < ApplicationController
+  before_action :find_artist, only: [:show, :edit, :update]
+
+  def index
+    @artists = Artist.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.create(artist_params)
+
+    redirect_to artists_path(@artist)
+  end
+
+  def edit
+
+  end
+
+  def update
+  @artist.update(artist_params)
+
+  redirect_to @artist
+  end
+
+private
+def artist_params
+  params.require(:artist).permit(:name, :bio)
+end
+
+def find_artist
+  @artist = Artist.find(params[:id])
+end
+  # def destroy do not need this for this lab
+  # end
+
+
+end
